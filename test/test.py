204d564d3a05ebaa2f7beb49f5399927e2b411dc
Chao-Quan-You <yoto@nctu.edu.tw>
2022-05-15 13:38:04 +0800

    feat: add testEdgeCases function

--- test/test.py
+++ test/test.py
@@ -41,3 +41,14 @@ def testSeveralDiscounts():
 
     book_list = [0, 1, 1, 2, 3, 4]
     assert price(book_list) == 8 + (8 * 5 * 0.75)
+
+def testEdgeCases():
+    book_list = [0, 0, 1, 1, 2, 2, 3, 4]
+    assert price(book_list) == 2 * (8 * 4 * 0.8)
+
+    book_list = [ 0, 0, 0, 0, 0, 
+                  1, 1, 1, 1, 1, 
+                  2, 2, 2, 2, 
+                  3, 3, 3, 3, 3, 
+                  4, 4, 4, 4 ]
+    assert price(book_list) == 3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8)
