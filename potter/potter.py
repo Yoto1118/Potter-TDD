169b59f08210803f5a49396700bbb7f89a6831f5
Chao-Quan-You <yoto@nctu.edu.tw>
2022-05-15 13:40:19 +0800

    fix: fix error that it's better to group 8 books as 2 4s than a 3 and a 5

--- potter/potter.py
+++ potter/potter.py
@@ -5,7 +5,8 @@ BOOK_PRICE = 8
 DISCOUNTS = {2: 0.95, 3: 0.90, 4: 0.80, 5: 0.75}
 
 def price(books):
-    groups = group_into_sets(books)
+    sets = group_into_sets(books)
+    groups = optimize_sets(sets)
     return sum(set_price(g) for g in groups)
 
 def group_into_sets(books):
@@ -19,3 +20,20 @@ def group_into_sets(books):
 
 def set_price(set_size):
     return BOOK_PRICE * set_size * DISCOUNTS.get(set_size, 1)
+
+def optimize_sets(groups):
+    # It's better to group 8 books as 2 4s than a 3 and a 5.
+    # Do this manually.
+    #
+    # This is hard coded, and there may well be edge cases
+    # for combinations other than (3 + 5) | (4 + 4).
+    # A solution that looks at the value of the discount per book
+    # would be more flexible.
+    set_sizes = Counter(groups)
+
+    while set_sizes[3] and set_sizes[5]:
+        set_sizes[3] -= 1
+        set_sizes[5] -= 1
+        set_sizes[4] += 2
+
+    return list(set_sizes.elements())
